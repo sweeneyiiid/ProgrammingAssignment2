5b07b5f0f6775c3618ec661778dcf68db47ed767
Dan Sweeney <dlswee@gmail.com>
2015-09-27 11:52:23 -0400

    second commit, changed indent to make more readable

--- cachematrix.R
+++ cachematrix.R
@@ -8,18 +8,18 @@
 ## stored with the original matrix to avoid re-computation
 makeCacheMatrix <- function(x = matrix()) {
   
-  x_inv <- NULL
+    x_inv <- NULL
   
-  set <- function(y) {
-    x <<- y
-    x_inv <<- NULL
-  }
-  get <- function() x
-  setinv <- function(inv) x_inv <<- inv
-  getinv <- function() x_inv
-  list(set = set, get = get,
-       setinv = setinv,
-       getinv = getinv)
+    set <- function(y) {
+        x <<- y
+        x_inv <<- NULL
+    }
+    get <- function() x
+    setinv <- function(inv) x_inv <<- inv
+    getinv <- function() x_inv
+    list(set = set, get = get,
+         setinv = setinv,
+         getinv = getinv)
 }
 
 
@@ -27,13 +27,13 @@ makeCacheMatrix <- function(x = matrix()) {
 ##    if so, store the inverse so it doesnt need to be recalculated
 ##    if not, just return the previously solved inverse
 cacheSolve <- function(x, ...) {
-  x_inv <- x$getinv()
-  if(!is.null(x_inv)) {
-    message("getting cached data")
-    return(x_inv)
-  }
-  data <- x$get()
-  x_inv <- solve(data)
-  x$setinv(x_inv)
-  x_inv
+    x_inv <- x$getinv()
+    if(!is.null(x_inv)) {
+        message("getting cached data")
+        return(x_inv)
+    }
+    data <- x$get()
+    x_inv <- solve(data)
+    x$setinv(x_inv)
+    x_inv
 }
